086f97014f39c06b772957d08a356cf342615d2e
Leila Chulani <leilachu7762@gmail.com>
2026-07-06 13:27:08 -0700

    Connect project to riot api and verify authentication

--- src/collect_matches.py
+++ src/collect_matches.py
@@ -1,1 +1,23 @@
-print("Project is working")
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("RIOT_API_KEY")
+
+if API_KEY is None:
+    raise ValueError("RIOT_API_KEY not found. Check your .env file.")
+
+print("API Key loaded succesfully.")
+
+headers = {
+    "X-Riot-Token": API_KEY
+}
+
+url = "https://na1.api.riotgames.com/lol/status/v4/platform-data"
+
+response = requests.get(url, headers=headers)
+
+print("Status code:", response.status_code)
+print(response.json())
